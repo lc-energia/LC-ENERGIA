cf7321d2268a56f92cb886826a92334a52ba0a89
Munaytu <125014435+Munaytu@users.noreply.github.com>
2024-04-04 18:21:05 +0200

    coreccion coreeo enviar form

--- mail/mail.php
+++ mail/mail.php
@@ -1,22 +1,35 @@
-<?
-$name = $_POST{'name'};
-$email = $_POST{'email'};
-$phone = $_POST{'phone'};
-$subject = $_POST{'subject'};
-$message = $_POST['message'];
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recoger los datos del formulario
+    $nombre = $_POST["name"];
+    $email = $_POST["email"];
+    $telefono = $_POST["phone"];
+    $indirizzo = $_POST["indirizzo"];
+    $immagine_aerea = $_POST["immagine_aerea_edificio"];
+    $consumo_energia = $_POST["consumo_energia"];
 
-$email_message = "
+    // Configurar los detalles del correo electrónico
+    $destinatario = "info@lc-energia.it"; // Cambia esto al correo electrónico al que deseas enviar el formulario
+    $asunto = "Nuevo mensaje de formulario";
 
-Name: ".$name."
-Email: ".$email."
-Phone: ".$phone."
-Subject: ".$subject."
-Message: ".$message."
+    // Construir el cuerpo del mensaje
+    $mensaje = "Nombre: $nombre\n";
+    $mensaje .= "Email: $email\n";
+    $mensaje .= "Teléfono: $telefono\n";
+    $mensaje .= "Indirizzo: $indirizzo\n";
+    $mensaje .= "Immagine aerea edificio (opzionale): $immagine_aerea\n";
+    $mensaje .= "Consumo kWh/annuo/ spesa mensile energia (opzionale): $consumo_energia\n";
 
-";
-
-mail ("name@youremail.com" , "New Message", $email_message);
-header("location: ../mail-success.html");
+    // Enviar el correo electrónico
+    if (mail($destinatario, $asunto, $mensaje)) {
+        echo "El formulario ha sido enviado correctamente.";
+    } else {
+        echo "Hubo un error al enviar el formulario.";
+    }
+} else {
+    echo "Error: Método no permitido.";
+}
 ?>
+
 
 
