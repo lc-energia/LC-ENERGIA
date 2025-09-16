bcfe6709e065dd837ed68d4e77e01a1f79632405
kapiwaraton <kapiwaraton@proton.me>
2025-09-16 16:37:37 -0400

    acrediti

--- src/app/accrediti/page.tsx
+++ src/app/accrediti/page.tsx
@@ -13,14 +13,22 @@ const AccreditationsPage = () => {
       <PageHeader title="Accrediti" breadcrumb="Accrediti" />
 
       <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
-          <div className="container text-center">
-              <div className="row justify-content-center">
-                  <div className="col-lg-6">
-                      <i className="bi bi-shield-fill-check display-1 text-primary"></i>
-                      <h3 className=" mb-0">{accreditation.title}</h3>
-                      <h4 className="mb-1">{accreditation.pageDescription}</h4>
-                      <p className="mb-4" dangerouslySetInnerHTML={{ __html: accreditation.description.split('.<br>').join('.<br><br>') }}></p>
-                      <Link href="/" className="btn btn-primary rounded-pill py-3 px-5">Torna alla homepage</Link>
+          <div className="container">
+              <div className="row g-5 align-items-center">
+                  <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                      <h1 className="display-5 mb-4">{accreditation.pageTitle}</h1>
+                      <p className="mb-4">{accreditation.pageDescription}</p>
+                      <ul className="list-group list-group-flush">
+                          {accreditation.description.split('.<br>').map((item, index) => (
+                              <li key={index} className="list-group-item"><i className="fa fa-check-circle text-primary me-2"></i>{item}</li>
+                          ))}
+                      </ul>
+                      <div className="mt-4">
+                        <Link href="/contact" className="btn btn-primary rounded-pill py-3 px-5">Richiedi un preventivo</Link>
+                      </div>
+                  </div>
+                  <div className="col-lg-6 wow fadeIn text-center" data-wow-delay="0.5s">
+                      <i className="bi bi-shield-fill-check display-1 text-primary" style={{ fontSize: '12rem' }}></i>
                   </div>
               </div>
           </div>
